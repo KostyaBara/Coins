75fdfa9e1842a5d7a6e9cdc89fd10bc269f32628
k.baranov <kostya_baranov@ukr.net>
2023-07-05 16:04:01 +0300

    row added

--- app/src/main/java/com/example/coins/ui/screens/details/CoinDetailsScreen.kt
+++ app/src/main/java/com/example/coins/ui/screens/details/CoinDetailsScreen.kt
@@ -1,16 +1,16 @@
 package com.example.coins.ui.screens.details
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
@@ -19,16 +19,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.nestedscroll.nestedScroll
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
-import com.example.coins.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -43,7 +40,7 @@ fun CoinDetailsScreen(
     val uiState = viewModel.uiState.collectAsState().value
     Scaffold(
         modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
-        topBar = { CoinDetailsTopNavBar(scrollBehavior = scrollBehavior) },
+        topBar = { CoinDetailsTopNavBar(scrollBehavior = scrollBehavior, onBackClick = onBackClick) },
     ) { innerPadding ->
         CoinCard(modifier = Modifier.padding(innerPadding))
     }
@@ -54,42 +51,47 @@ fun CoinCard(
     modifier: Modifier = Modifier,
 
     ) {
-
+Text("Be water, my friend")
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CoinDetailsTopNavBar(
+    onBackClick: () -> Unit,
     scrollBehavior: TopAppBarScrollBehavior,
     modifier: Modifier = Modifier,
 ) {
     CenterAlignedTopAppBar(
         scrollBehavior = scrollBehavior,
         title = {
-            Icon(
-                imageVector = R.,
-                contentDescription = null,
-                tint = iconColor,
-                modifier = Modifier
-                    .clickable { onClick }
-                    .align(alignment = Alignment.CenterVertically)
-                    .padding(8.dp)
-            )
+            Row {
+                IconButton(
+                    onClick = { onBackClick() },
+                    modifier = Modifier
+                        .size(40.dp)
+                        .align(alignment = Alignment.CenterVertically)
+                ) {
+                    Icon(
+                        imageVector = Icons.Filled.ArrowBack,
+                        contentDescription = null,
+                        tint = Color.Black
+                    )
+                }
+
+                Spacer(modifier = Modifier.width(20.dp))
+
+                Text(
+                    text = ("Coin"),
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier
+                        .padding(vertical = 8.dp)
+                )
+            }
         }
-
-                Spacer (width = 12.dp)
-
-                Text (
-                text = title,
-        style = NrsTheme.typography.title1Medium,
-        color = textColor,
-        textAlign = TextAlign.Center,
-        modifier = Modifier
-            .padding(vertical = 8.dp)
-    )
-    modifier = modifier
     )
 }
+
+
 
 
 
